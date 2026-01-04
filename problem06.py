0f0f97b59b4bae3d5edabd1810992459d9bb440a
Zhamilya <kambaraalievva@gmail.com>
2026-01-04 03:07:59 -0600

    Organized folders

--- problem06.py
+++
@@ -1,9 +0,0 @@
-def clip(x, low, high):
-    if x < low: return low
-    if x > high: return high
-    return x
-
-# Примеры
-tests = [5, -2, 10, 15, 0]
-for t in tests:
-    print(f"clip({t}, 0, 10) = {clip(t, 0, 10)}")
--- problem07.py
+++
@@ -1,9 +0,0 @@
-table = []
-for i in range(1, 11):
-    row = ""
-    for j in range(1, 11):
-        row += f"{i*j:4}"
-    table.append(row)
-
-for r in table:
-    print(r)
